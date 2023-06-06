06f42d7885d21d91be97a75e3f57f323d4adf0d1
Gustavo Moraes <gustavo.moliveira01@gmail.com>
2023-06-06 13:02:08 -0300

    unit tests

--- src/main/java/com/taurus/financeapi/test/Calculadora.java
+++ src/main/java/com/taurus/financeapi/test/Calculadora.java
@@ -1,2 +1,22 @@
-package com.taurus.financeapi.test;public class Calculadora {
+package com.taurus.financeapi.test;
+
+public class Calculadora {
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisão por zero não é permitida");
+        }
+        return a / b;
+    }
 }
--- src/test/java/com/taurus/financeapi/FinanceApiApplicationTests.java
+++ src/test/java/com/taurus/financeapi/FinanceApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.taurus.financeapi;
 
+import com.taurus.financeapi.test.Calculadora;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,30 @@ class FinanceApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testeSoma() {
+		int resultado = Calculadora.somar(2, 3);
+		Assertions.assertEquals(5, resultado);
+	}
+
+	@Test
+	public void testeSubtracao() {
+		int resultado = Calculadora.subtrair(5, 3);
+		Assertions.assertEquals(2, resultado);
+	}
+
+	@Test
+	public void testeMultiplicacao() {
+		int resultado = Calculadora.multiplicar(2, 3);
+		Assertions.assertEquals(6, resultado);
+	}
+
+	@Test
+	public void testeDivisao() {
+		double resultado = Calculadora.dividir(10, 2);
+		Assertions.assertEquals(5.0, resultado, 0.001);
 	}
 
 }
